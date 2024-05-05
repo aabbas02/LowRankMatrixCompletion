87130fd86da975515441835eca873b9f3d8a27d7
Ahmed Abbasi <aabbasi1@iastate.edu>
2024-05-05 17:52:06 -0500

    removed a lot of files

--- main/r10/p005_ProjGD/pltFrmData.m
+++
@@ -1,5 +0,0 @@
-clc
-close all
-clear all
-dir = pwd;
-saveFigCntrl(4,dir)
--- main/r20/p01/pltFrmDataCntrl.m
+++
@@ -1,9 +0,0 @@
-clc
-close all
-clear all
-dir = pwd;
-% For linux, replace '\' with '/'
-cd ('C:\Users\aabbasi1\OneDrive - Iowa State University\Desktop\FLRMC\altGDMin2ndPaper')
-addpath(genpath('.\functions'));
-numData = 9;
-saveFigCntrl(numData,dir)
