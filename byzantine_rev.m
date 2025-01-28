349f3a0a4adf084c951b37c56126f70f41ef7116
Abbasi <aabbasi1@iastate.edu>
2025-01-28 14:14:43 -0600

    commit

--- byzantine_rev.m
+++ byzantine_rev.m
@@ -1,12 +1,13 @@
 function byz_rev=byzantine_rev(L_byz,stored_delta,C1,attck)
    [size1, size2, ~] = size(stored_delta);
-   s = mean(stored_delta,3);
+   %s = mean(stored_delta,3);
+   s = sum(stored_delta,3);
    byz_rev=zeros(size1,size2,L_byz);
    for i = 1 : L_byz
       if attck 
         byz_rev(:,:,i) = -C1*s;
       else
-        byz_rev(:,:,i) = 0 + randn(size1,size2);
+        byz_rev(:,:,i) = 10 + 1e-3*randn(size1,size2);
       end
    end
    
