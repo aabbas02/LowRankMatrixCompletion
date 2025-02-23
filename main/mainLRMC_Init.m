8df7615c5e7178af0a535237865b0b78c7eb6a5f
Ahmed Ali Abbasi <aabbasi1@iastate.edu>
2025-02-23 17:46:29 -0600

    commit

--- main/mainLRMC_Init.m
+++ main/mainLRMC_Init.m
@@ -11,37 +11,32 @@ cd(dir)
 r = 10;
 n = 1000;
 q = 1000;
-p = 0.1;
 same = 0;
-r_ = ones(1,numBlocks)*(n/numBlocks);
 MC = 1;
-real = 1;
 %------------------------
-[Ustr,X] = getMovieLens(5);
+real = 1;
+[Ustr,X,p] = getMovieLens(r);
 n = size(X,1);
 q = size(X,2);
-% generate rank-r X*
+%-------------------------------
 %Ustr = orth(randn(n,r));
 %Bstr = randn(r,q);
 %X  = Ustr*Bstr;
 %----------------------------------------
-%numBlocksTry_ = [10,20,30,40,50,60,70,80,90,100];
-numBlocksTry_ = [100,120,140,160,180,200]/2;
+%numBlocksTry_ = 5:10:200;
+numBlocksTry_ = [200,300];
+%numBlocksTry_ = [200, 220, 240, 260, 280, 300];
+%numBlocksTry_ = [20,40,60,80,100,120,140,160,180,200];
+%numBlocksTry_ = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,80,90,100,110,120,130,140,150];
 % generate rank-r X*
 %Ustr = orth(randi(1000,[n,r]));
 %Bstr = randn(r,q);
 %X  = Ustr*Bstr;
-%X = rand(n,q);
-%[Ustr,Sstr,Vstr] = svd(X,'econ');
-%Ustr = Ustr(:,1:r);
-%Sstr = Sstr(1:r,1:r);
-%Vstr = Vstr(:,1:r);
-%X = Ustr*Sstr*Vstr';
 SDU0 = zeros(length(numBlocksTry_),MC);
 SDU0Cllps = zeros(length(numBlocksTry_),MC);
 SDU0Perm = zeros(length(numBlocksTry_),MC);
 fill = "mean";
-fill = "both";
+%fill = "both";
 %------------------------
 %-----------------------
 for i = 1 : length(numBlocksTry_)
@@ -49,8 +44,8 @@ for i = 1 : length(numBlocksTry_)
     for mc = 1 : MC
         rowIdxPerm = cell(q,1);
         XcolPerm = cell(q, 1); 
-        XzerosPerm = zeros(n,q);
-        XzerosCllps = zeros(n,q);
+        XzerosPerm = sparse(n,q);
+        XzerosCllps = sparse(n,q);
         %if same
         %    pi_map = get_permutation_r(n,r_);
         %end
@@ -83,16 +78,16 @@ for i = 1 : length(numBlocksTry_)
                 %AkCllps_{k}(s,start:stop) = ones(1,r_(s));
             end
         end
-        [U0,~,~,] = svd(Xzeros,"econ");
-        U0 = U0(:,1:r);
-        SDU0(i,mc) = norm(Ustr - U0*(U0'*Ustr));
+        %[U0,~,~,] = svds(Xzeros,r);
+        %U0 = U0(:,1:r);
+        %SDU0(i,mc) = norm(Ustr - U0*(U0'*Ustr));
         %-----------------------------------
         %norm(Xzeros - XzerosCllps,'fro')
-        [U0Cllps,~,~] = svd(XzerosCllps,"econ");
+        [U0Cllps,~,~] = svds(XzerosCllps,r);
         U0Cllps = U0Cllps(:,1:r);
         SDU0Cllps(i,mc) = norm(Ustr - U0Cllps*(U0Cllps'*Ustr));
         %---------------------------------------
-        [U0Perm,~,~] = svd(XzerosPerm,"econ");
+        [U0Perm,~,~] = svds(XzerosPerm,r);
         U0Perm = U0Perm(:,1:r);
         SDU0Perm(i,mc) = norm(Ustr - U0Perm*(U0Perm'*Ustr));    
         mc
@@ -106,13 +101,13 @@ plotRslts(SDU0, SDU0Cllps, SDU0Perm,n,q,r,p,numBlocksTry_,MC,same,fill);
 
 function plotRslts(SDU0, SDU0Cllps, SDU0Perm,n,q,r,p,numBlocks_,MC,same,fill)
     figure;
-    plot(numBlocks_,SDU0, ...
-        'DisplayName', 'SDVals U^{(0)}', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
-    hold on;
+    %plot(numBlocks_,SDU0, ...
+    %    'DisplayName', 'SDVals U^{(0)}', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
     plot(numBlocks_,SDU0Cllps, ...
-        'DisplayName', 'SDVals U^{(0)}-Cllps', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
+        'DisplayName', 'SDVals Cllps', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
+    hold on
     plot(numBlocks_,SDU0Perm, ...
-        'DisplayName', 'SDVals U^{(0)}-Perm', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
+        'DisplayName', 'SDVals Naive', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
     grid("on")
     xticks(numBlocks_);
     %-------------------------------
@@ -121,10 +116,11 @@ function plotRslts(SDU0, SDU0Cllps, SDU0Perm,n,q,r,p,numBlocks_,MC,same,fill)
            'Interpreter', 'Latex', 'FontSize',11)
     %--------------------------------
     legend('Interpreter', 'Latex', 'Fontsize', 9);
-    ylabel("$SD(U^{(0)},U^*)$","FontSize",14,'Interpreter','Latex')
+    %ylabel("$SD(U^{(0)},U^*)$","FontSize",14,'Interpreter','Latex')
+    ylabel("Initialization Error", "FontSize",11,"Interpreter","Latex")
     xlabel('number of blocks', 'FontSize',14, 'Interpreter','Latex')
     stringTitle = ['LRMC_Init_MC_', num2str(MC), ...
-                   '_n_', num2str(n), '_q_', num2str(q), '_r_', num2str(r), '_p_', num2str(p), '_same_',num2str(same),'_fill_',num2str(fill)];
+                   '_n_', num2str(n), '_q_', num2str(q), '_r_', num2str(r), '_p_', num2str(p),'_numBlocks_', num2str(max(numBlocks_)), '_same_',num2str(same),'_fill_',num2str(fill)];
     
     savefig([stringTitle, '.fig']);
 end
@@ -162,45 +158,28 @@ function [Xzeros, rowIdx, colIdx, Xcol, Xrow] = processMatrix(X, n, q, p,real)
     end
 end
 %---
-%plotRslts(SDVals_sLcl, SDVals_Perm, SDVals_UnPerm,n,q,r,m,numBlocks,MC,same);
-function plotRsltsLRMCInit(numBlocks_, SDVals_,n,q,r,m,MC,same)
-    figure;
-    %SDVals_
-    %SDVals_UnPerm = sum(SDVals_UnPerm,1)/MC;
-    %SDVals_sLcl = sum(SDVals_sLcl,1)/MC;
-    semilogy(numBlocks_,SDVals_, ...
-        'DisplayName', 'SDVals-Spectral Init LRMC', 'LineWidth', 1.45, 'Marker', 'o', 'MarkerSize', 7);
-    hold on;
-    %semilogy(SDVals_Perm, ...
-    %    'DisplayName', 'SDVals (Naive)', 'LineWidth', 1.45, 'Marker', 'square', 'MarkerSize', 7);
-    %
-    %semilogy(SDVals_UnPerm, ...
-    %    'DisplayName', 'SDVals (Unpermuted)', 'LineWidth', 1.45, 'Marker', 'x', 'MarkerSize', 7);
-    %grid on
-    
-    
-    title("n = " + n + ", q = " + q +...
-          ", r = " + r + ", m = " + m + ", num Blocks = " + numBlocks +  ", MC = " + MC + ", same = " + same, ...
-           'Interpreter', 'Latex', 'FontSize',14)
-    
-    legend('Interpreter', 'Latex', 'Fontsize', 9);
-    ylabel("$SD(U,U^*)$","FontSize",14,'Interpreter','Latex')
-    xlabel('Iterations (t)', 'FontSize',14, 'Interpreter','Latex')
-    stringTitle = ['MtrxSnsngPerm_MC_', num2str(MC), ...
-                   '_n_', num2str(n), '_q_', num2str(q), '_r_', num2str(r), '_m_', num2str(m), '_numBlocks_',num2str(numBlocks), '_same_',num2str(same)];
-    
-    savefig([stringTitle, '.fig']);
-end
-function [Ustr,X] = getMovieLens(r)
-    A = readmatrix("ratings.xlsx");
+function [Ustr,X,p] = getMovieLens(r)
+    %A = readmatrix("ratings.xlsx");
+    %load("ratings1M.mat");
+    %A = ratings1M;
+    load("ratings10M.mat")
     n = max(A(:,1));
     q = max(A(:,2));
-    X = zeros(n,q);
+    X = sparse(n,q);
+    num = 0;
+    disp(num)
     for k = 1 : size(A,1)
         i = A(k,1);
         j = A(k,2);
         X(i,j) = A(k,3);
+        num = num + 1;
+        if mod(k,10000) == 0
+            size(A,1),k
+        end
     end
-    [Ustr,~,~] = svd(X,"econ");
+    p = num/(n*q);
+    X = X';
+    [Ustr,~,~] = svds(X,r);
     Ustr = Ustr(:,1:r);
+    1
 end
