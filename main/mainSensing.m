e40442e5995a579b48b4cfd99e151be2afad947f
Ahmed Ali Abbasi <aabbasi1@iastate.edu>
2025-03-06 13:33:46 -0600

    commit

--- main/mainSensing.m
+++ main/mainSensing.m
@@ -9,14 +9,15 @@ addpath(genpath('.\functionsMtrxSnsng'));
 addpath(genpath('.\utils'));
 cd(dir)    
 %---------------------------------
+altMin  = 1;
 r = 5;
 n = 600;
 q = 1000;
 m = 100;
 numBlocks = 20;   %effectively, m_new = numBlocks
 r_ = ones(1,numBlocks)*(m/numBlocks);
-T = 200;
-MC = 1;
+T = 50;
+MC = 125;
 % generate rank-r X*
 Ustr = orth(randn(n,r));
 Bstr = randn(r,q);
@@ -33,7 +34,7 @@ MPerm = zeros(n,q);
 SDVals_UnPerm = zeros(MC,T+1);
 SDVals_sLcl = zeros(MC,T+1);
 SDVals_Perm = zeros(MC,T+1);
-same = 0;
+same = 1;
 %if same
 %    pi_map = get_permutation_r(m,r_);
 %end
@@ -51,12 +52,12 @@ for mc = 1 : MC
         ykPerm_{k} = yk_{k}(pi_map);
         AkCllps_{k} = zeros(length(r_),n);
         ykCllps_{k} = zeros(length(r_),1);
-            for i = 1 : length(r_)
-                start = sum(r_(1:i)) - r_(i) + 1;
-                stop  = sum(r_(1:i));
-                AkCllps_{k}(i,:) = sum(Ak_{k}(start:stop,:));
-                ykCllps_{k}(i) = sum(ykPerm_{k}(start:stop,:));
-            end
+        for i = 1 : length(r_)
+            start = sum(r_(1:i)) - r_(i) + 1;
+            stop  = sum(r_(1:i));
+            AkCllps_{k}(i,:) = sum(Ak_{k}(start:stop,:));
+            ykCllps_{k}(i) = sum(ykPerm_{k}(start:stop,:));
+        end
         MCllps(:, k) = AkCllps_{k}'*ykCllps_{k};
     end
     [U0,~,~,] = svd(M,"econ");
@@ -69,10 +70,10 @@ for mc = 1 : MC
     %U0Perm = U0Perm(:,1:r);
     %------------------------------
     updtP = 0;
-    SDVals_UnPerm(mc,:) = altGDMin_MtrxSensingPerm(Ak_, yk_,Ak_, yk_, U0,r,T,Ustr,r_,updtP,same);
+    SDVals_UnPerm(mc,:) = altGDMin_MtrxSensingPerm(Ak_, yk_,Ak_, yk_, U0,r,T,Ustr,r_,updtP,same,altMin);
     updtP = 1;
     SDVals_sLcl(mc,:) = altGDMin_MtrxSensingPerm(Ak_, ykPerm_, ...
-        AkCllps_, ykCllps_, U0Cllps,r,T,Ustr,r_,updtP,same);
+    AkCllps_, ykCllps_, U0Cllps,r,T,Ustr,r_,updtP,same,altMin);
     %SDVals_Perm(mc,:) = altGDMin_MtrxSensing(Ak_, ykPerm_, U0Perm,r,T,Ustr);
     mc
 end
