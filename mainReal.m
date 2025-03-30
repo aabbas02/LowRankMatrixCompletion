ef26a63843913ee20e20ea1e2600f20424d8938c
Ahmed Ali Abbasi <aabbasi1@iastate.edu>
2025-03-30 15:01:24 -0500

    Update mainReal.m

--- mainReal.m
+++ mainReal.m
@@ -4,8 +4,8 @@ clear all
 close all
 clc
 r = 3;  % rank
-Telem = 10000;  % number of iterations
-Tgm = 10000;
+Telem = 5000;  % number of iterations
+Tgm = 5000;
 mu = 2;  % parameter for projection operator
 attck = 0;
 L = 20;   % number of nodes
@@ -68,41 +68,41 @@ for mc = 1 : MC
     %---------------------------------
     % GD step GM with check and attacks
     %---------------------------------
-    % U_t_prev = U;
-    % nrmsPrev = sqrt(sum(U_t_prev.^2,2));
-    % for t = 1 : 1 * Tgm
-    %     [stored_delta] = nodeLoopReal(U_t_prev,q,n,r,rowIdx,Ycol,L,rowsJ,Ycol_);
-    %     byz_rev=byzantine_rev(L_byz,stored_delta,C1,attck);
-    %     for j = 1 : L_byz
-    %         idx_ = randi([1,L]);
-    %         %idx = 1;
-    %         stored_delta(:,:,idx_) = byz_rev(:,:,j);
-    %     end
-    %     I_t = [];
-    %     for i = 1 : L
-    %         U_temp = U_t_prev - eta_gm * stored_delta(:,:,i);
-    %         nrmsTemp = sqrt(sum(U_temp.^2,2)); %norm(U_temp')
-    %         accept_update = all(nrmsTemp <= (1 - 0.4 / (kappa_tilde^2)) *nrmsPrev + 0.4 * mu * sqrt(r / n));
-    %         % If all conditions are met, accept the update for this node
-    %         if accept_update
-    %             I_t = [I_t, i];  % Add index i to the set I_t
-    %         else
-    %             if mod(t,100) == 0
-    %                 fprintf('GM. Node %d rejected based on norm condition.\n', i);
-    %             end
-    %         end
-    %     end
-    %     accepted_deltas = stored_delta(:,:,I_t);
-    %     Grad_U = do_gm_vec(accepted_deltas);
-    %     U_cap=U_t_prev - eta_gm * Grad_U;
-    %     [Q1,~] = qr(U_cap);
-    %     U_t_prev=Q1(:,(1:r));
-    %     nrmsPrev = sqrt(sum(U_t_prev.^2,2));
-    %     error(3,t,mc) = getErr(U_t_prev,r,q,L,rowsJ,Ycol_,Y,idx);
-    %     if mod (t,100) == 0
-    %         fprintf('GM.  n = %d, q = %d, r = %d, p = %f, L = %d, L_byz = %d, C1 = %d. Iteration %d, Rel. Error: %f\n', n,q,r,p, L,L_byz, C1, t, error(3,t,mc));
-    %     end
-    % end
+    U_t_prev = U;
+    nrmsPrev = sqrt(sum(U_t_prev.^2,2));
+    for t = 1 : 1 * Tgm
+        [stored_delta] = nodeLoopReal(U_t_prev,q,n,r,rowIdx,Ycol,L,rowsJ,Ycol_);
+        byz_rev=byzantine_rev(L_byz,stored_delta,C1,attck);
+        for j = 1 : L_byz
+            idx_ = randi([1,L]);
+            %idx = 1;
+            stored_delta(:,:,idx_) = byz_rev(:,:,j);
+        end
+        I_t = [];
+        for i = 1 : L
+            U_temp = U_t_prev - eta_gm * stored_delta(:,:,i);
+            nrmsTemp = sqrt(sum(U_temp.^2,2)); %norm(U_temp')
+            accept_update = all(nrmsTemp <= (1 - 0.4 / (kappa_tilde^2)) *nrmsPrev + 0.4 * mu * sqrt(r / n));
+            % If all conditions are met, accept the update for this node
+            if accept_update
+                I_t = [I_t, i];  % Add index i to the set I_t
+            else
+                if mod(t,100) == 0
+                    fprintf('GM. Node %d rejected based on norm condition.\n', i);
+                end
+            end
+        end
+        accepted_deltas = stored_delta(:,:,I_t);
+        Grad_U = do_gm_vec(accepted_deltas);
+        U_cap=U_t_prev - eta_gm * Grad_U;
+        [Q1,~] = qr(U_cap);
+        U_t_prev=Q1(:,(1:r));
+        nrmsPrev = sqrt(sum(U_t_prev.^2,2));
+        error(3,t,mc) = getErr(U_t_prev,r,q,L,rowsJ,Ycol_,Y,idx);
+        if mod (t,100) == 0
+            fprintf('GM.  n = %d, q = %d, r = %d, p = %f, L = %d, L_byz = %d, C1 = %d. Iteration %d, Rel. Error: %f\n', n,q,r,p, L,L_byz, C1, t, error(3,t,mc));
+        end
+    end
     %----------------------------------
     %GD step Krum with check and attack
     %----------------------------------
