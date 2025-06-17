7cffcb1ab52640145fb399073d43b5c1959dc78a
Ahmed Ali Abbasi <aabbasi1@iastate.edu>
2025-06-16 22:38:01 -0500

    included 2d phase transition plot

--- utils/plotRsltsPhsTrnstn2d.m
+++ utils/plotRsltsPhsTrnstn2d.m
@@ -1,23 +1,41 @@
-function plotRsltsPhsTrnstn(numBlocks_, ...
+function plotRsltsPhsTrnstn2d(numBlocks_, rVals, ...
                    prbAltGDMin, ...
                    prbAltMin,...
                    n,q,r,m,numBlocks,MC,same,T_LS,eta_c)
     curDir = pwd;
     cd ..
     cd figsMtrxSensing
-    figure;
-    plot(numBlocks_, prbAltGDMin, 'DisplayName', 'AltGDMin', 'LineWidth', 1.05, 'Marker', 'o', 'MarkerSize', 3)
-    hold on
-    plot(numBlocks_, prbAltMin, 'DisplayName', 'AltMin', 'LineWidth', 1.05, 'Marker', 'square', 'MarkerSize', 3)
-    grid on        
-    title("n = " + n + ", q = " + q +...
+    figure
+    %grid on        
+
+    % Display using imagesc
+    imagesc(prbAltGDMin);
+
+    % x ticks
+    xticks(numBlocks_);
+
+    % y ticks
+    yticks(rVals)
+    
+    % Set colormap to grayscale
+    colormap(gray);
+    
+    % Ensure color scaling is [0, 1]
+    caxis([0 1]);
+    
+    % Add colorbar for reference
+    colorbar;
+    
+    % Optional: remove axis ticks if you want a clean image display
+    axis off;
+    title("AltGDMin. n = " + n + ", q = " + q +...
           ", r = " + r + ", m = " + m + ", MC = " + MC + ", $\eta_c = $ " + eta_c, ...
            'Interpreter', 'Latex', 'FontSize',11)
     
-    legend('Interpreter', 'Latex','Fontsize', 9);
-    ylabel("$\Pr[SD(U^{(T)},U^*)] < 10^{-10}$","FontSize",14,'Interpreter','Latex')
+    %legend('Interpreter', 'Latex', 'Fontsize', 9);
+    %ylabel("$\Pr[SD(U^(T),U^*)] < 10^{-10}$","FontSize",14,'Interpreter','Latex')
     xlabel('Number of blocks', 'FontSize',14, 'Interpreter','Latex')    
-    stringTitle = ['PhaseTrnstn_MC_', num2str(MC), ...
+    stringTitle = ['PhaseTrnstn2d_MC_', num2str(MC), ...
                    '_n_', num2str(n), '_q_', num2str(q), '_r_', num2str(r), ... 
                    '_m_', num2str(m), '_numBlocks_',num2str(numBlocks), ...
                    '_same_',num2str(same),'_T_LS_', num2str(T_LS), '_eta_c_',num2str(eta_c)];
